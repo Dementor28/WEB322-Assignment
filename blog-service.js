a5b62385e868ce4c420efc1bb214a69987520054
Abdullah <abdullah1@myseneca.ca>
2023-02-19 20:51:50 -0500

    Added Various Functionalities

--- blog-service.js
+++ blog-service.js
@@ -1,116 +1,140 @@
-
 /*********************************************************************************
-*  WEB322 – Assignment 02
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
-*  (including 3rd party web sites) or distributed to other students.
-* 
-*  Name:Abdullah Student ID: 152158200 Date: 03 FEB 2023
-*
-*  Online (Cyclic) Link: https://funny-ray-cuff-links.cyclic.app/about
-*
-********************************************************************************/
+ *  WEB322 – Assignment 03
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
+ *  (including 3rd party web sites) or distributed to other students.
+ *
+ *  Name:Abdullah Student ID: 152158200 Date: 03 FEB 2023
+ *
+ *  Online (Cyclic) Link: https://funny-ray-cuff-links.cyclic.app/about
+ *
+ ********************************************************************************/
 
-const fs = require("fs");
 
-//GLobal arrays
+//Arrays
 var posts = [];
 var categories = [];
 
-function initialize() {
+var errormsg = "No Results Returned";
+
+const { promises, resolve } = require("dns");
+const fs = require("fs"); // required at the top of your module
+
+module.exports.initialize = function () {
+  fs.readFile("./data/posts.json", (err, posts_data) => {
+    if (err) {
+      return Promise.reject("unable to read the File posts.json");
+    }
+    posts = JSON.parse(posts_data);
+  });
+
+  fs.readFile("./data/categories.json", (err, categories_data) => {
+    if (err) {
+      return Promise.reject("unable to read the File categories.json");
+    }
+    categories = JSON.parse(categories_data);
+  });
+
+  return Promise.resolve();
+};
+
+
+module.exports.getAllPosts = function () {
+  var promise = new Promise((resolve, reject) => {
+    if (posts.length === 0) {
+      reject(errormsg);
+    }
+    resolve(posts);
+  });
+  return promise;
+};
+
+
+module.exports.getPublishedPosts = function () {
+  var publishedPost = [];
+  var promise = new Promise((resolve, reject) => {
+    for (var i = 0; i < posts.length; i++) {
+      if (posts[i].published == true) {
+        publishedPost.push(posts[i]);
+      }
+    }
+    if (posts.length === 0) {
+      reject(errormsg);
+    }
+    resolve(publishedPost);
+  });
+  return promise;
+};
+
+module.exports.addPost = function (postData) {
   return new Promise(function (resolve, reject) {
-    fs.readFile("./data/posts.json", "utf8", (err, data) => {
-      if (err) {
-        // console.error(err);
-        reject("Unable to read posts.json file");
-        return;
+    if (postData.published == undefined) {
+      postData.published = false;
+    } else {
+      postData.published = true;
+    }
+    postData.id = posts.length + 1;
+    posts.push(postData);
+    resolve(posts);
+  });
+};
+
+
+module.exports.getPostsByCategory = function (category) {
+  var postsByCategory = [];
+
+  return new Promise((resolve, reject) => {
+    for (var i = 0; i < posts.length; i++) {
+      if (posts[i].category == category) {
+        postsByCategory.push(posts[i]);
+      }
+    }
+    if (postsByCategory.length > 0) {
+      resolve(postsByCategory);
+    } else {
+      reject(errormsg);
+    }
+  });
+};
+
+//Get Post by category By using Filter
+// module.exports.getPostsByCategory = (category) => {
+//   return new Promise(function(resolve, reject) {
+//       const read = fs.readFileSync('./data/posts.json');
+//       var Posts = JSON.parse(read);
+//       var filter = Posts.filter(a => a.category == category);
+//       if (filter.length > 0) {
+//           resolve(filter);
+//       } else {
+//           reject("No Matching Category Found!");
+//       }
+//   });
+// };
+
+
+module.exports.getPostsByMinDate = (minDateStr) => {
+  return new Promise((resolve, reject) => {
+      const read = fs.readFileSync('./data/posts.json');
+      var publishedPosts = JSON.parse(read);
+      var filter = publishedPosts.filter(a => new Date(a.postDate) >= new Date(minDateStr));
+      if (filter.length > 0) {
+          resolve(filter);
+      } else {
+          reject(errormsg);
       }
 
-      const post = JSON.parse(data);
-      // posts.push(post);
-      posts = post.slice();
-      // console.log(posts);
-
-      fs.readFile("./data/categories.json", "utf8", (err, data) => {
-        if (err) {
-          // console.error(err);
-          reject("Unable to read categories.json file");
-          return;
-        }
-
-        const category = JSON.parse(data);
-        categories = category.slice();
-        // console.log(categories);
-        // console.log(categories);
-
-        //resolve method
-        resolve({ posts, categories });
-      });
-    });
   });
 }
-// ************************************************************************************************
 
-
-function getAllPosts() {
+module.exports.getPostById = (id) => {
   return new Promise((resolve, reject) => {
-    initialize()
-      .then(({ posts }) => {
-        if (posts.length === 0) {
-          reject("no results available");
-          return;
-        }
-        resolve(posts);
-      })
-      .catch((error) => {
-        reject(error);
-      });
+      const read = fs.readFileSync('./data/posts.json');
+      var publishedPosts = JSON.parse(read);
+      var filter = publishedPosts.filter(a => a.id == id);
+      if (filter.length > 0) {
+          resolve(filter);
+      } else {
+          reject(errormsg);
+      }
+
   });
 }
-// ************************************************************************************************
-
-
-function getPublishedPosts() {
-  return new Promise((resolve, reject) => {
-    // Just a trial code
-    // const posts = getAllPosts().filter((post) => post.published === true);
-    initialize()
-      .then(({ posts }) => {
-        let publishedPosts = posts.filter((post) => post.published === true);
-        if (publishedPosts.length === 0) {
-          reject("No results returned");
-          return;
-        }
-        resolve(publishedPosts);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
-}
-// ************************************************************************************************
-
-
-function getCategories() {
-  return new Promise((resolve, reject) => {
-    initialize()
-      .then(({ categories }) => {
-        if (categories.length === 0) {
-          reject("no results available");
-          return;
-        }
-        resolve(categories);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
-}
-// ************************************************************************************************
-
-
-//making initialize available for other modules
-module.exports.initialize = initialize;
-module.exports.getAllPosts = getAllPosts;
-module.exports.getPublishedPosts = getPublishedPosts;
-module.exports.getCategories = getCategories;
-// ************************************************************************************************
--- server.js
+++ server.js
@@ -1,134 +1,170 @@
-
 /*********************************************************************************
-*  WEB322 – Assignment 02
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
-*  (including 3rd party web sites) or distributed to other students.
-* 
-*  Name:Abdullah Student ID: 152158200 Date: 03 FEB 2023
-*
-*  Online (Cyclic) Link: https://funny-ray-cuff-links.cyclic.app/about
-*
-********************************************************************************/
+ *  WEB322 – Assignment 03
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
+ *  (including 3rd party web sites) or distributed to other students.
+ *
+ *  Name:Abdullah Student ID: 152158200 Date: 03 FEB 2023
+ *
+ *  Online (Cyclic) Link: https://funny-ray-cuff-links.cyclic.app/about
+ *
+ ********************************************************************************/
 
 // express module
-var express = require("express");
-var app = express();
-var fs = require("fs");
-app.use(express.static("public"));
-const blogservice = require("./blog-service");
-// ************************************************************************************************
+const express = require("express");
+//Assignment 3
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+//************************************************************************************************
 
+const upload = multer(); // no { storage: storage } since we are not using disk storage
+const path = require("path");
+const app = express();
+var fs = require("fs");
+app.use(express.static("./public"));
+app.use(express.static("./views"));
+const blogService = require("./blog-service");
+
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+
+// ************************************************************************************************
 //route on the root of url
 app.get("/", (req, res) => {
-  res.redirect("/about");
+  res.sendFile(path.join(__dirname + "/views/about.html"));
 });
-// ************************************************************************************************
 
-
-//Returning about.html page when url has "/about" 
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html");
+  res.sendFile(path.join(__dirname + "/views/about.html"));
 });
-// ************************************************************************************************
 
-
-//returning response to confirm routes are wroking
 app.get("/blog", (req, res) => {
-  blogservice
+  blogService
     .getPublishedPosts()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-
-  // Some experimenting with the code below...
-  // fs.readFile('./data/posts.json', (err, data) => {
-
-  //   if(err) {
-  //     console.error(err);
-  //     return;
-  //   }
-
-  //   const posts = JSON.parse(data);
-  //   const publishedPosts = posts.filter(post => post.published === true);
-
-  //   // console.log(publishedPosts);
-  //   return JSON.stringify(publishedPosts);
-
-  //   res.send(JSON.stringify(publishedPosts));
-
-  // });
-  // res.send("TODO: get all posts who have published == true");
-});
-// ************************************************************************************************
-
-app.get("/posts", (req, res) => {
-  blogservice
-    .getAllPosts()
     .then((data) => {
       res.json(data);
     })
     .catch((err) => {
-      res.json("Cannot get published posts");
+      res.json(err);
     });
 });
-// ************************************************************************************************
+
+app.get("/posts", function (req, res) {
+  let Promise = null;
+
+  // by category query
+  if (req.query.category) {
+    Promise = blogService.getPostsByCategory(req.query.category);
+    // by mindate Query
+  } else if (req.query.minDate) {
+    Promise = blogService.getPostsByMinDate(req.query.minDate);
+    //all posts
+  } else {
+    Promise = blogService.getAllPosts();
+  }
+
+  Promise
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.error(err); // log the error
+      res.status(500).send("An error occurred: " + err);
+    });
+});
+
+// Add the "/post/value" route
+app.get("/post/:id", (req, res) => {
+  blogService
+    .getPostById(req.params.id)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.error(err); // log the error
+      res.send("ID does not exist: " + err);
+    });
+});
 
 
 app.get("/categories", (req, res) => {
-  blogservice
+  blogService
     .getCategories()
     .then((data) => {
       res.json(data);
     })
     .catch((err) => {
-      res.json("Cannot get categories");
+      res.json(err);
     });
-
-  // Some Experimenting with the code below
-  //Reads the file asynchoronously
-  //   fs.readFile('./data/categories.json', (err, data) => {
-  //           if(err) {
-  //             res.writeHead(500, { 'Content-Type': 'application/json'});
-  //             res.end(JSON.stringify({error: 'Error reading posts file'}));
-  //             return;
-  //           }
-
-  //           //Parsing the data
-  //           const categories = JSON.parse(data);
-
-  //           res.writeHead(200, { 'Content-Type': 'application/json' });
-  //           res.end(JSON.stringify(categories));
-  // });
-
-  // res.send("Returns all the categories here");
 });
-// ************************************************************************************************
 
-
-app.get("*", (req, res) => {
-  res.status(404).json("BIG ERROR 404!...Page Not Found");
+//GET/posts/add
+app.get("/posts/add", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/addPost.html"));
 });
-//************************************************************************************************
 
-// listening on port 8080
-app.set("port", process.env.PORT || 8080);
+// the post/add route
+app.post("/posts/add", upload.single("featureImage"), function (req, res) {
+  let streamUpload = (req) => {
+    return new Promise((resolve, reject) => {
+      let stream = cloudinary.uploader.upload_stream((error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      });
 
-const HTTP_PORT = process.env.PORT || 8080;
+      streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
+  };
 
-blogservice
+  async function upload(req) {
+    let result = await streamUpload(req);
+    console.log(result);
+    return result;
+  }
+  upload(req).then((uploaded) => {
+    req.body.featureImage = uploaded.url;
+
+    // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+    // to correctly add the new blog post before redirecting the user to the /posts route
+    blogService
+      .addPost(req.body)
+      .then(function (data) {
+        res.redirect("/posts");
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
+});
+
+app.use("*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname + "/views/sorry404.html"));
+});
+
+blogService
   .initialize()
   .then(() => {
     app.listen(HTTP_PORT, onHttpStart);
   })
-  .catch((error) => {
-    console.error(error);
-    console.log("Failed to initialize server");
+  .catch((err) => {
+    console.log("error", err);
   });
+const HTTP_PORT = process.env.PORT || 8080;
 
 function onHttpStart() {
   console.log("Express http server listening on port " + HTTP_PORT);
 }
 // ************************************************************************************************
+
+//*******************************************Assignment 3*********************************************
+cloudinary.config({
+  cloud_name: "devvtx5mb",
+  api_key: "245521392787718",
+  api_secret: "9YrmTclJ5Lz13g9nnHL7yZQ3Pnc",
+  secure: true,
+});
+
+//************************************************************************************************
